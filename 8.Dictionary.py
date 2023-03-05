ff2def5bb248fe2374e92db9fbfbe4523c8d4d10
Oybek Tursunov <123294338+oybektmcoder@users.noreply.github.com>
2023-03-05 16:33:17 +0500

    8.dict
    8

---
+++ 8.Dictionary.py
@@ -0,0 +1,68 @@
+#car_1={
+#     'model':'audio',
+#     'years':'1997',
+#    'make':'germany'
+#    }
+#print(car_1['years'])
+
+#student_1={
+#    'name':'Oybek',
+#    'fname':'Tursunov',
+#    'years':'2005'
+#    }
+#print(
+#      f"Name:{student_1['name'].title()} \nSurname:{student_1['fname'].title()} \nYears:{student_1['years']}"
+#      )
+#student_1['spicific']='math'
+#student_1['gruop']='206'
+#student_1['name']="Aybek"
+#print(
+#      f"Name:{student_1['name'].title()} \nSurname:{student_1['fname'].title()} \nYears:{student_1['years']} \nSpc:{student_1['spicific'].title()} \nGroup:{'group'}"
+#      )
+##
+#del student_1['spicific']
+#print(student_1)
+
+#Get
+#user={'Asad':'2001','Jasur':'2002'}
+#print(user.get('Assad','Error name'))
+
+#father={
+#      'Name':'Nurali'  
+#        }
+#mather={
+#      'Name':'Sayyora'
+#        }
+#
+#User={
+#      'Name':'Oybek',
+#      'Fname':father['Name'], 
+#     'Mname':mather['Name']
+#     }
+#
+#print(User)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
